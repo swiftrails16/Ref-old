7486b710f94f6e455d46f5ca15a319e5859d4f80
System Administrator <root@smiths-mbp.mynetworksettings.com>
2024-03-07 23:47:27 -0500

    03/07/24 | 11:24pm | Smith Dsouza

---
+++ src/com/service/UserService.java
@@ -0,0 +1,22 @@
+package com.service;
+
+import java.util.List;
+
+import com.beans.UserBean;
+import com.beans.TrainException;
+
+public interface UserService {
+
+	public UserBean getUserByEmailId(String userEmailId) throws TrainException;
+
+	public List<UserBean> getAllUsers() throws TrainException;
+
+	public String updateUser(UserBean customer);
+
+	public String deleteUser(UserBean customer);
+
+	public String registerUser(UserBean customer);
+	
+	public UserBean loginUser(String username, String password) throws TrainException;
+
+}
